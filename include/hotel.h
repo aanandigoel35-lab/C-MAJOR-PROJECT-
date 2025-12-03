3e4e2cb2f3bbebdfe696161fccbcdc997022c5e0
aanandigoel35-lab <aanandigoel35@gmail.com>
2025-12-03 15:31:52 +0530

    hotel.h

--- include/hotel.h
+++ include/hotel.h
@@ -1,2 +1,17 @@
-#include <stdio.h>
-#include <string.h>
+#ifndef HOTEL_H
+#define HOTEL_H
+
+#define MAX_ROOMS 5
+#define MAX_NAME 50
+
+struct Reservation {
+    int room_no;
+    char guest_name[MAX_NAME];
+    int days;
+};
+
+// Function declarations
+void bookRoom(struct Reservation hotel[], int booked[]);
+void viewReservations(struct Reservation hotel[], int booked[]);
+
+#endif
